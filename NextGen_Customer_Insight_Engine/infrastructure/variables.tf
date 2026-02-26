a8f718bf64f43aefdc9e493a226e90ab78709f24
aitechgurus-developer <satya.it.careers@gmail.com>
2026-02-26 16:49:34 -0500

    Update variables.tf

--- NextGen_Customer_Insight_Engine/infrastructure/variables.tf
+++ NextGen_Customer_Insight_Engine/infrastructure/variables.tf
@@ -4,4 +4,3 @@ variable "location" { default = "East US" }
 variable "storage_replication" { default = "LRS" }
 variable "sql_sku" { default = "GP_S_Gen5_1" }
 variable "sql_admin_password" { sensitive = true }
-variable "openai_model_name" { default = "gpt-4o-mini" }
