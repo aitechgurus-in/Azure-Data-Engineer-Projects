ebbbd8cbf01094ea7ae74739d1567fcc22a8e063
aitechgurus-developer <satya.it.careers@gmail.com>
2026-02-26 16:59:55 -0500

    Update 01_bronze_to_silver.py

--- NextGen_Customer_Insight_Engine/databricks/01_bronze_to_silver.py
+++ NextGen_Customer_Insight_Engine/databricks/01_bronze_to_silver.py
@@ -1,14 +1,13 @@
 from pyspark.sql.functions import col, regexp_replace, current_timestamp
 
-# Read raw chats from Landing Zone
-df = spark.read.json("abfss://external-source@stnextgendev.dfs.core.windows.net/chats.json")
+# Read Raw JSON Chats
+df = spark.read.json("abfss://external-source@stnextgendev.dfs.core.windows.net/sample_chats.json")
 
-# Mask PII (Emails) for Security Compliance
-# Interview Tip: Explain you redact PII before sending data to LLMs
+# Mask PII (Emails)
 silver_df = df.withColumn(
     "chat_masked", 
     regexp_replace(col("chat_history"), r"[\w\.-]+@[\w\.-]+", "[REDACTED_EMAIL]")
 ).withColumn("processed_at", current_timestamp())
 
-# Write to Silver Delta Table
+# Write to Silver Delta
 silver_df.write.format("delta").mode("overwrite").save("abfss://silver@stnextgendev.dfs.core.windows.net/chat_cleaned")
