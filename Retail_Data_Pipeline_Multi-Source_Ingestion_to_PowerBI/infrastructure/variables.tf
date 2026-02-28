29ec90e412b2366a73dd7d86b31855e62a0fdb4a
aitechgurus-developer <satya.it.careers@gmail.com>
2026-02-28 10:07:14 -0500

    Update variables.tf

--- Retail_Data_Pipeline_Multi-Source_Ingestion_to_PowerBI/infrastructure/variables.tf
+++ Retail_Data_Pipeline_Multi-Source_Ingestion_to_PowerBI/infrastructure/variables.tf
@@ -1,29 +1,21 @@
-variable "project_name" {
-  type    = string
-  default = "retail-project"
-}
-
-variable "environment" {
-  type        = string
-  description = "dev or prod"
+variable "resource_group_name" {
+  type = string
 }
 
 variable "location" {
-  type    = string
-  default = "West US 2"
+  type = string
 }
 
-variable "sql_server_name" {
-  type    = string
+variable "server_name" {
+  type = string
 }
 
 variable "database_name" {
-  type    = string
+  type = string
 }
 
 variable "admin_username" {
-  type    = string
-  default = "adminuser"
+  type = string
 }
 
 variable "admin_password" {
@@ -31,12 +23,6 @@ variable "admin_password" {
   sensitive = true
 }
 
-variable "sql_sku" {
-  type        = string
-  description = "Basic, GP_S_Gen5_1, etc."
-}
-
-variable "backup_redundancy" {
-  type        = string
-  description = "LRS, GRS, or ZRS"
+variable "workload_env" {
+  type = string
 }
