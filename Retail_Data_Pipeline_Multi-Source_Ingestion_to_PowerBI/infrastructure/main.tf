3be8d6a4ef3d469f7825305eb303764d2af0ab68
aitechgurus-developer <satya.it.careers@gmail.com>
2026-02-28 10:09:23 -0500

    Update main.tf

--- Retail_Data_Pipeline_Multi-Source_Ingestion_to_PowerBI/infrastructure/main.tf
+++ Retail_Data_Pipeline_Multi-Source_Ingestion_to_PowerBI/infrastructure/main.tf
@@ -2,40 +2,61 @@ provider "azurerm" {
   features {}
 }
 
-# 1. Resource Group (Unique per environment)
+# Auto-fetch your current public IP for the "Add current client IP" requirement
+data "http" "my_public_ip" {
+  url = "https://ifconfig.me/ip"
+}
+
+# 1. Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${var.project_name}-${var.environment}"
+  name     = var.resource_group_name
   location = var.location
 }
 
-# 2. SQL Server
+# 2. Azure SQL Server
 resource "azurerm_mssql_server" "sql_server" {
-  name                         = "${var.sql_server_name}-${var.environment}"
+  name                         = var.server_name
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = var.admin_username
   administrator_login_password = var.admin_password
+  public_network_access_enabled = true
 }
 
-# 3. SQL Database
+# 3. Azure SQL Database (Basic + Sample Data)
 resource "azurerm_mssql_database" "sql_db" {
   name           = var.database_name
   server_id      = azurerm_mssql_server.sql_server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
-  sku_name       = var.sql_sku
-  storage_account_type = var.backup_redundancy
+  sku_name       = "Basic" 
+  sample_name    = "AdventureWorksLT"
+  storage_account_type = "LRS"
 
   tags = {
-    environment = var.environment
-    workload    = "Retail-POC"
+    environment = var.workload_env
   }
 }
 
-# 4. Firewall Rule (Allow Azure Services)
+# 4. Networking: Allow Azure Services
 resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
   name             = "AllowAzureServices"
   server_id        = azurerm_mssql_server.sql_server.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
+}
+
+# 5. Networking: Add Current Client IP
+resource "azurerm_mssql_firewall_rule" "client_ip" {
+  name             = "ClientIPAddress"
+  server_id        = azurerm_mssql_server.sql_server.id
+  start_ip_address = chomp(data.http.my_public_ip.response_body)
+  end_ip_address   = chomp(data.http.my_public_ip.response_body)
+}
+
+# 6. Security: Microsoft Defender
+resource "azurerm_mssql_server_security_alert_policy" "defender" {
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mssql_server.sql_server.name
+  state               = "Enabled"
 }
